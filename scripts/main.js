0de5b91eaca94844ad640e24424ae94863b63151
ivanbrime <ivanjogadorcaro@gmail.com>
2025-09-20 12:47:54 -0300

    jogodavelha

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,23 @@
 function urlBuilder(number){
-    number+= 
-    number=number.padStart(2,'0');
-    return `imagens/card${number}.jpeg`;
+    // Convert number to string and pad with '0' if necessary
+    let strNum = number.toString().padStart(2, '0');
+    return `imagens/card${strNum}.jpeg`;
 }
+
 let card = new CardManager(urlBuilder);
-let board = new BoardManager("board", 5 ,card)
+let board = new BoardManager("board", 5, card);
 
-let menu = document.getElementById('menu')
-let select = document.getElementById('numCards')
-let start = document.getElementById('start')
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let start = document.getElementById('start');
 
+for(let i = 4; i <= 10; i += 2) {
+    let op = document.createElement('option');
+    op.value = i;    
+    op.innerHTML = i; 
+    select.appendChild(op);
+}
+    start.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+    
